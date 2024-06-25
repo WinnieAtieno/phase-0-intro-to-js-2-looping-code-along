4ef0f8d621777fa4d989c7201880648fb6604443
WinnieAtieno <winnieotieno773@gmail.com>
2024-06-25 18:50:24 +0300

    completed loop lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+function writeCards (arr,event){
+    let newArr = [];
+    for(let i= 0; i<arr.length;i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr
+
+}                                                                                              
+   
+
+
+writeCards(["Guadalupe", "Ollie", "Aki"],'suprise gift!')
+
+// create a function that counts down using a while loop
+
+function countDown(num){
+    let count = 0;
+    while(num >= count){
+        console.log(num)
+        num--
+    }
+
+}
+countDown(10)
+
+
+
